54451f491f63305e742aa70164b3f55d1b6ee262
Riccardo Pagnozzi <riccardo.pagnozzi@gmail.com>
2023-06-14 21:03:02 +0200

    Cleaning up

--- README.md
+++ README.md
@@ -11,7 +11,8 @@ Set the new screensaver language to Japanese
 On the first run it copies the official Word of the day screensaver and installs a second copy 
 Afterwards it clears the list and replaces it with the content of your new csv 
 
-Csv should be three columns wide
+**Csv should be three columns wide** Kanji, Kana, Meaning for my usecase  
+_This is a "hack" I use on my system, as always double check what you git clone_
 
 Here's the result ![E1xgpBfXoAQwW-M](https://user-images.githubusercontent.com/50523188/120007580-584ab980-bfda-11eb-9301-3d94862f0b99.jpg)
 
--- wotd.py
+++ wotd.py
@@ -2,70 +2,62 @@ import plistlib
 import csv
 import sys
 import os
+import logging
 
 if os.path.isdir("/Library/Screen Savers/Word of the Dayrr.saver"):
-    print("Modified WOTD exists, proceeding")
+    logging.debug("Modified WOTD exists, proceeding")
 else:
-    print("Modified WOTD does not exist, copying original")
+    logging.debug("Modified WOTD does not exist, copying original")
     try:
-        os.system("cp -R '/System/Library/Screen Savers/Word of the Day.saver' 'Word of the Dayrr.saver'")
+        os.system("cp -R '/System/Library/Screen Savers/Word of the Day.saver' 'Word of the Dayrr.saver'") # Need to do some mktemp-like stuff here
         os.system("open 'Word of the Dayrr.saver'")
         print("Rerun script after adding WOTD screensaver")
     except:
         print("Check for original WOTD path")
     finally:
         exit(1)
+
 rows = []
 try:
     csv_file = sys.argv[1]
 except IndexError:
     print('Provide csv as argument')
     exit(1)
-# open the CSV file and read its contents
+
 with open(csv_file, 'r', encoding='utf-8') as file:
-    # create a CSV reader object
     reader = csv.reader(file)
-    # loop through each row in the file
     for row in reader:
-        # check that the row contains exactly three columns
         if len(row) == 3:
-            # add the row as a list of three elements to the rows list
             rows.append(row)
         else:
-            # handle rows with incorrect number of columns here
+            # !=3 to be implemented
             pass
 
 with open('/Library/Screen Savers/Word of the Dayrr.saver/Contents/Resources/WordLists/WotD_info_DJR.plist', 'rb') as tmpPlist:
     tmpPlistOpen = plistlib.load(tmpPlist, fmt=None, dict_type=dict)
-#check for the DCSWotDentriesDate dictionary and removes it if it exists
+
 if 'DCSWotDEntriesByDate' in tmpPlistOpen.keys():
-    print('DCSWotDEntriesByDate dictionary exists')
     del tmpPlistOpen['DCSWotDEntriesByDate']
-    print('DCSWotDentriesDate dictionary removed')
-#check for the DCSWotDEntries array
+    logging.debug('DCSWotDentriesDate dictionary removed')
+
 if 'DCSWotDEntries' in tmpPlistOpen.keys():
-    print('DCSWotDEntries array exists')
+    logging.debug('DCSWotDEntries array exists')
 else:
     tmpPlistOpen['DCSWotDEntries'] = []
-    print('DCSWotDEntries array created')    
-    # clears the array if not empty
+    logging.debug('DCSWotDEntries array created')
+
 if len(tmpPlistOpen['DCSWotDEntries']) > 0:
     tmpPlistOpen['DCSWotDEntries'].clear()
-    print('DCSWotDEntries array cleared')
-    #loop through the rows and create a dictionary for each row
+    logging.debug('DCSWotDEntries array cleared')
+
 for r in rows:
-    #create a dictionary for each row
     tmpDict = {}
-    #add the first column to the dictionary as DCSWotDEntryHeadword
     tmpDict['DCSWotDEntryHeadword'] = r[0]
-    #add the second column to the dictionary as DCSWotDEntrySecondaryHeadword
     tmpDict['DCSWotDEntrySecondaryHeadword'] = r[1]
-    #add the third column to the dictionary as DCSWotDEntryDefinition
     tmpDict['DCSWotDEntrySense'] = r[2]
-    #append the dictionary to the DCSWotDEntries array
     tmpPlistOpen['DCSWotDEntries'].append(tmpDict)
-    print(tmpPlistOpen['DCSWotDEntries'])
-# write the modified plist file
+
 with open('/Library/Screen Savers/Word of the Dayrr.saver/Contents/Resources/WordLists/WotD_info_DJR.plist', 'wb') as tmpPlist:
     plistlib.dump(tmpPlistOpen, tmpPlist)
+
 print("New screensaver for you")
