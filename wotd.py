8f866ad65f331deaa08770491564bf9a2247b965
Riccardo Pagnozzi <riccardo.pagnozzi@gmail.com>
2023-06-15 18:59:25 +0200

    Somewhat readable code

--- wotd.py
+++ wotd.py
@@ -4,60 +4,75 @@ import sys
 import os
 import logging
 
-if os.path.isdir("/Library/Screen Savers/Word of the Dayrr.saver"):
-    logging.debug("Modified WOTD exists, proceeding")
-else:
-    logging.debug("Modified WOTD does not exist, copying original")
+logging.basicConfig(level=logging.DEBUG)
+entries = []
+
+
+def copy_open_first_run():
+    if os.path.isdir("/Library/Screen Savers/Word of the Dayrr.saver"):
+        logging.debug("Modified WOTD exists, proceeding")
+    else:
+        logging.debug("Modified WOTD does not exist, copying original")
+        try:
+            os.system("cp -R '/System/Library/Screen Savers/Word of the Day.saver' '/tmp/Word of the Dayrr.saver'")
+            os.system("open '/tmp/Word of the Dayrr.saver'")
+            print("Rerun script after adding WOTD screensaver")
+        except Exception as e:
+            print(f"Caught exception: {e}")
+        finally:
+            exit(1)
+
+
+def csv_case():
     try:
-        os.system("cp -R '/System/Library/Screen Savers/Word of the Day.saver' 'Word of the Dayrr.saver'") # Need to do some mktemp-like stuff here
-        os.system("open 'Word of the Dayrr.saver'")
-        print("Rerun script after adding WOTD screensaver")
-    except:
-        print("Check for original WOTD path")
-    finally:
-        exit(1)
+        csv_file = sys.argv[1]
+    except IndexError:
+        raise ValueError('Provide csv as argument')
 
-rows = []
-try:
-    csv_file = sys.argv[1]
-except IndexError:
-    print('Provide csv as argument')
-    exit(1)
+    with open(csv_file, 'r', encoding='utf-8') as file:
+        reader = csv.reader(file)
+        entries.extend([row for row in reader if len(row) == 3])
 
-with open(csv_file, 'r', encoding='utf-8') as file:
-    reader = csv.reader(file)
-    for row in reader:
-        if len(row) == 3:
-            rows.append(row)
-        else:
-            # !=3 to be implemented
-            pass
 
-with open('/Library/Screen Savers/Word of the Dayrr.saver/Contents/Resources/WordLists/WotD_info_DJR.plist', 'rb') as tmpPlist:
-    tmpPlistOpen = plistlib.load(tmpPlist, fmt=None, dict_type=dict)
+def manage_plist():
+    plist_file_path = '/Library/Screen Savers/Word of the Dayrr.saver/Contents/Resources/WordLists/WotD_info_DJR.plist'
 
-if 'DCSWotDEntriesByDate' in tmpPlistOpen.keys():
-    del tmpPlistOpen['DCSWotDEntriesByDate']
-    logging.debug('DCSWotDentriesDate dictionary removed')
+    with open(plist_file_path, 'rb') as plist_file:
+        plist_content = plistlib.load(plist_file, fmt=None, dict_type=dict)
 
-if 'DCSWotDEntries' in tmpPlistOpen.keys():
-    logging.debug('DCSWotDEntries array exists')
-else:
-    tmpPlistOpen['DCSWotDEntries'] = []
-    logging.debug('DCSWotDEntries array created')
+    if 'DCSWotDEntriesByDate' in plist_content.keys():
+        del plist_content['DCSWotDEntriesByDate']
+        logging.debug('DCSWotDentriesDate dictionary removed')
 
-if len(tmpPlistOpen['DCSWotDEntries']) > 0:
-    tmpPlistOpen['DCSWotDEntries'].clear()
-    logging.debug('DCSWotDEntries array cleared')
+    if 'DCSWotDEntries' in plist_content.keys():
+        logging.debug('DCSWotDEntries array exists')
+    else:
+        plist_content['DCSWotDEntries'] = []
+        logging.debug('DCSWotDEntries array created')
 
-for r in rows:
-    tmpDict = {}
-    tmpDict['DCSWotDEntryHeadword'] = r[0]
-    tmpDict['DCSWotDEntrySecondaryHeadword'] = r[1]
-    tmpDict['DCSWotDEntrySense'] = r[2]
-    tmpPlistOpen['DCSWotDEntries'].append(tmpDict)
+    if len(plist_content['DCSWotDEntries']) > 0:
+        plist_content['DCSWotDEntries'].clear()
+        logging.debug('DCSWotDEntries array cleared')
 
-with open('/Library/Screen Savers/Word of the Dayrr.saver/Contents/Resources/WordLists/WotD_info_DJR.plist', 'wb') as tmpPlist:
-    plistlib.dump(tmpPlistOpen, tmpPlist)
+    plist_content['DCSWotDEntries'] = [
+        {
+            'DCSWotDEntryHeadword': entry[0],
+            'DCSWotDEntrySecondaryHeadword': entry[1],
+            'DCSWotDEntrySense': entry[2]
+        }
+        for entry in entries
+    ]
 
-print("New screensaver for you")
+    with open(plist_file_path, 'wb') as plist_file:
+        plistlib.dump(plist_content, plist_file)
+        logging.debug('Screensaver updated')
+
+
+def main():
+    copy_open_first_run()
+    csv_case()
+    manage_plist()
+
+
+if __name__ == '__main__':
+    main()
